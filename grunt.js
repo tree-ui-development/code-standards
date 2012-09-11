478e6a73e42009e18f2bf065b0b63c9d62ab7f55
Rob Cherny <rcherny@gmail.com>
2012-09-11 16:02:09 -0400

    Changes to branding and Roundarch Isobar references.

--- grunt.js
+++ grunt.js
@@ -7,26 +7,30 @@ module.exports = function(grunt) {
             files: ['_front_matter.txt', 'general.html', 'markup.html', 'css.html', 'javascript.html', 'performance.html', 'browsers.html', 'seo.html', 'codeReviews.html', 'appendices.html', 'revisionHistory.html']
         }
     }
+
     function addBasePaths(basePath, fileArray){
         var output = [];
+
             fileArray.forEach(function(i){
-                // output.push(basePath.replace('{pref}', pref) + i);
                 output.push(basePath + i);
             });
+            
         return output;
     };
 
     grunt.initConfig({
         concat: {
-            src: ((function(){
-                var files = [];
+            dist : {
+                src: ((function(){
+                    var files = [];
+          
+                    files = addBasePaths(metas.en.path, metas.en.files);
 
-                files = addBasePaths(metas.en.path, metas.en.files);
+                    return files;
 
-                return files;
-
-            })()),
-           dest: 'index.html'
+                })()),
+               dest: 'index.html'
+            }
         }
     });
 
